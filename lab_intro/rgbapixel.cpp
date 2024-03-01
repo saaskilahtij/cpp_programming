501348ff684e406125dc75af39e31c693f6655b4
Johan <saaskilahtijohan@gmail.com>
2024-03-01 05:47:49 -0500

    Added lab_intro

---
+++ lab_intro/rgbapixel.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file rgbapixel.cpp
+ * Implementation of the RGBAPixel class for the EasyPNG library.
+ *
+ * @author Chase Geigle
+ * @date Spring 2012
+ */
+#include "rgbapixel.h"
+
---
+++ lab_intro/rgbapixel.h
@@ -0,0 +1,11 @@
+/**
+ * @file rgbapixel.h
+ * Definition of the RGBAPixel class for the EasyPNG library.
+ *
+ * @author Chase Geigle
+ * @date Spring 2012
+ */
+class RGBAPixel
+{
+
+};
